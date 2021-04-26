cec15a80feea371395e6aae9f9859fc88a977975
Neon <danielemmanuel257@gmail.com>
2021-04-26 09:02:12 +0100

    added typescript, tailwind,and other dependencies

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div className="w-full h-64 bg-green-500">
+      <Button>click me</Button>
+    </div>
+  );
+}
+
+export default App;
