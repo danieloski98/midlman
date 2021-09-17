7148a136fa0b0bc48a91af4c32cf52e45bf327a6
Neon <danielemmanuel257@gmail.com>
2021-09-17 16:34:03 +0100

    added the deposit

--- src/Features/Dashboard/Pages/Payment/Deposit.tsx
+++ src/Features/Dashboard/Pages/Payment/Deposit.tsx
@@ -1,6 +1,31 @@
 import React from 'react'
 import {Select, InputGroup, InputLeftElement, Input} from '@chakra-ui/react'
 import { FiSearch, FiTrash } from 'react-icons/fi'
+import { useQuery } from 'react-query'
+import { url } from '../../../../Utils/URL'
+import LoadingModal from '../../../Modals/LoadingModal'
+
+interface IDeposits {
+    status: string;
+    _id: string;
+    user: string;
+    depositor: string;
+    amount: number;
+    createdAt: string;
+    updatedAt: string;
+}
+
+// get deposits
+const getDeposits = async() => {
+    const request = await fetch(`${url}/transfer`);
+    const json = await request.json()
+
+    if (!request.ok) {
+        throw new Error("An error Occured")
+    } else {
+        return json;
+    }
+}
 
 const data = [
     {
@@ -44,23 +69,65 @@ const data = [
 const color = (value: string) => {
     switch(value) {
         case 'Pending': {
-            return 'yellow'
+            return '#F2994A'
         }
         case 'Successful': {
-            return 'Green'
+            return '#00A69C'
         }
         case 'Failed': {
-            return 'red'
+            return '#94316D'
         }
     }
 }
 
 export default function Deposit() {
     const [status, setStatus] = React.useState('Pending');
+    const [loading, setLoading] = React.useState(true);
+    const [deposits, setDeposits] = React.useState([] as IDeposits[])
+    const [text, setText] = React.useState('Loading Deposits...')
+
+    // query
+    const { refetch } = useQuery('getTransfers', getDeposits, {
+        onSuccess: (data) => {
+            console.log(data)
+            setDeposits(data.response)
+            setLoading(false);
+            setText("")
+        },
+        onError: (error) => {
+            console.log(error)
+            setLoading(false);
+            setText('')
+        }
+    });
+
+    const changeStatus = async(index: number, status: string) => {
+        setLoading(true);
+        setText("Updating deposit...");
+        const request = await fetch(`${url}/transfer/edit/${deposits[index]._id}`, {
+            method: 'put',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ status, depositor: deposits[index].depositor, amount: deposits[index].amount })
+        })
+        const json = await request.json();
+        setLoading(false);
+        setText("")
+
+        if (!request.ok) {
+            alert(json.message);
+        }else {
+            alert(json.message);
+            await refetch()
+        }
+        
+    }
 
     return (
         <div className="w-full px-8 h-auto flex flex-col">
-            <h3 className="text-xl font-Poppins-Medium">Deposit History</h3>
+            <LoadingModal open={loading} onClose={() => setLoading(false)} text="Loading Deposits..." />
+            <h3 className="text-xl font-Poppins-Medium mt-6">Deposit History</h3>
 
             {/* header */}
 
@@ -95,7 +162,10 @@ export default function Deposit() {
                                 <p className="w-56 text-sm font-Poppins-Medium">Transaction ID</p>
                             </th>
                             <th className="bg-white">
-                                <p className="w-56">Portal Account</p>
+                                <p className="w-56 text-sm font-Poppins-Medium">user ID</p>
+                            </th>
+                            <th className="bg-white">
+                                <p className="w-56">Depositor</p>
                             </th>
                             <th className="bg-white">
                                 <p className="w-56">Amount</p>
@@ -116,23 +186,24 @@ export default function Deposit() {
                     </thead>
                     <tbody>
                         {
-                            data.map((item, index) => (
+                            deposits.map((item, index) => (
                                 <tr key={index.toString()} className="font-Poppins-Medium text-xs">
-                                    <td>{item.transaction_id}</td>
-                                    <td>{item.portalAccount}</td>
+                                    <td>{item._id}</td>
+                                    <td>{item.user}</td>
+                                    <td>{item.depositor}</td>
                                     <td>{item.amount}</td>
-                                    <td>{item.date}</td>
+                                    <td>{item.createdAt}</td>
                                     {/* <td>{item.account_number}</td> */}
                                     <td>
-                                        <Select value={status} onChange={(e) => setStatus(e.target.value)} color={color(status)} size="xs" width="full">
+                                        <Select value={item.status} onChange={(e) => { changeStatus(index, e.target.value)}} color={color(item.status)} size="xs" width="full">
                                             <option value="Pending">Pending</option>
-                                            <option value="Approved">Successful</option>
-                                            <option value="Declined">Failed</option>
+                                            <option value="Successful">Successful</option>
+                                            <option value="Failed">Failed</option>
                                         </Select>
                                     </td>
                                     <td>
-                                        <p className="text-red-500 text-xs font-Poppins-Bold flex items-center">
-                                            <FiTrash size={20} color="red" />
+                                        <p className="text-red-300 text-xs font-Poppins-Bold flex items-center">
+                                            <FiTrash size={20} />
                                             <span className="ml-2">Delete</span>
                                         </p>
                                     </td>
