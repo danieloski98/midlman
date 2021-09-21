36dd67d2b47fed4e52e6d00a6f8d9e2192512728
Neon <danielemmanuel257@gmail.com>
2021-09-21 12:58:10 +0100

    done the withdrawal part

---
+++ src/Types/Withdrawals.ts
@@ -0,0 +1,9 @@
+export interface IWithdrawal {
+    status: string;
+    _id: string;
+    user: string;
+    amount: number;
+    reference: string;
+    createdAt: string;
+    updatedAt: string;
+}
